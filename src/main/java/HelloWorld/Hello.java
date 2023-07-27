c052769df4d288314cbe688901f50398ea2427d2
Columbus <eric90510@gmail.com>
2023-07-27 15:28:45 +0800

    java data type

--- src/main/java/HelloWorld/Hello.java
+++ src/main/java/HelloWorld/Hello.java
@@ -3,6 +3,11 @@ package HelloWorld;
 public class Hello {
     public static void main(String[] args) {
         //System.out.println("Hello world");
+        Integer age = 10;
+        Float weight = 66.6f;
+        String name = "鍋鍋";
+        Character a = 'A';
+        Boolean adult = false;
         Person P = new Person();
         P.hello();
     }
