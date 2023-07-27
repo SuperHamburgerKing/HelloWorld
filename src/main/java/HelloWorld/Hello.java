6b96eaf6e63f5660788c1e48ab0c5c53bd6e57cd
Columbus <eric90510@gmail.com>
2023-07-27 15:59:35 +0800

    add bmi method

--- src/main/java/HelloWorld/Hello.java
+++ src/main/java/HelloWorld/Hello.java
@@ -3,12 +3,10 @@ package HelloWorld;
 public class Hello {
     public static void main(String[] args) {
         //System.out.println("Hello world");
-        Integer age = 10;
-        Float weight = 66.6f;
-        String name = "鍋鍋";
-        Character a = 'A';
-        Boolean adult = false;
-        Person P = new Person();
-        P.hello();
+       Person p = new Person();
+       p.height = 1.77f;
+       p.weight = 74.4f;
+       p.hello();
+        System.out.println(p.bmi());
     }
 }
--- src/main/java/HelloWorld/Person.java
+++ src/main/java/HelloWorld/Person.java
@@ -3,8 +3,14 @@ package HelloWorld;
 import java.sql.SQLOutput;
 
 public class Person {
+    Float height;
+    Float weight;
     public void hello(){
         System.out.println("Hello world");
+    }
+    public float bmi(){
+        float bmi = weight/(height*height);
+        return bmi;
     }
 
 }
