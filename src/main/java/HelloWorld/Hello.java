a2f3a014e76d5b9fa54496287041ea55733865b4
Columbus <eric90510@gmail.com>
2023-07-27 16:26:03 +0800

    add constuctor

--- src/main/java/HelloWorld/Hello.java
+++ src/main/java/HelloWorld/Hello.java
@@ -3,7 +3,7 @@ package HelloWorld;
 public class Hello {
     public static void main(String[] args) {
         //System.out.println("Hello world");
-       Person p = new Person();
+       Person p = new Person(1.76f,74.4f);
        p.height = 1.77f;
        p.weight = 74.4f;
        p.hello();
--- src/main/java/HelloWorld/Person.java
+++ src/main/java/HelloWorld/Person.java
@@ -3,8 +3,17 @@ package HelloWorld;
 import java.sql.SQLOutput;
 
 public class Person {
-    Float height;
-    Float weight;
+    float height;
+    float weight;
+    String name;
+    public Person(float height,float weight){
+        this.height = height;
+        this.weight = weight;
+    }
+    public Person(String name,float height,float weight){
+        this(height, weight);
+        this.name = name;
+    }
     public void hello(){
         System.out.println("Hello world");
     }
